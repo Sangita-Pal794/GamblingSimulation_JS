7edc88f16b16265469421b799f3232ee8ad129a5
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-18 10:14:57 +0530

    To check resign condition

--- Utility.js
+++ Utility.js
@@ -1,29 +1,31 @@
-class UtilityGamblerSimulation{
- dailyStake = 100
- bet = 1
+class UtilityGamblerSimulation {
+    dailyStake = 100
+    bet = 1
 
- constructor(win, lose){
-    this.win = win;
-    this.lose = lose;
-}
-checkWinOrLoose(){
-    return Math.floor(Math.random() * Math.floor(1));
-}
-gambleWinOrLoose(){
-    let result = this.checkWinOrLoose();
-
-    if (result == 1) {
-        this.win += 1;
-        this.dailyStake += 1;
-        console.log( "You won 1 dollar")
+    constructor(win, lose) {
+        this.win = win;
+        this.lose = lose;
     }
-    if (result == 0) {
-        this.lose += 1;
-        this.dailyStake -= 1;
-        console.log("You lost 1 dollar")
+    checkWinOrLoose() {
+        return Math.floor(Math.random() * Math.floor(1));
+    }
+    gambleWinOrLoose() {
+        let result = this.checkWinOrLoose();
+
+        while (this.dailyStake <= 150 && this.dailyStake >= 50) {
+            if (result == 1) {
+                this.win += 1;
+                this.dailyStake += 1;
+                console.log("You won 1 dollar")
+            }
+            if (result == 0) {
+                this.lose += 1;
+                this.dailyStake -= 1;
+                console.log("You lost 1 dollar")
+            }
+
+        }
     }
 
 }
-
-}
-module.exports = new UtilityGamblerSimulation(0,0);
+module.exports = new UtilityGamblerSimulation(0, 0);
