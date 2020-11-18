4f5fecde05e4bb692db2fdc4ed037d84aad6add7
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-18 09:25:16 +0530

    Start with stake 100 & bet

---
+++ GamblerSimulation.js
@@ -0,0 +1,3 @@
+const Util = require('./Utility');
+console.log("Welcome to Gambling simulation!");
+console.log("stake:",Util.stake,"bet:",Util.bet);
---
+++ Utility.js
@@ -0,0 +1,10 @@
+class UtilityGamblerSimulation{
+ stake = 100
+ bet = 1
+contructor()
+{
+    this.stake=stake;
+    this.bet=bet;
+ }
+}
+module.exports = new UtilityGamblerSimulation();
